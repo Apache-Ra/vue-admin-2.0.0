3888a84e9e5f88ffb1b0629de494a4a56cb6e94b
Apache-Ra <summer.vip.20@qq.com>
2018-12-24 14:14:41 +0800

    添加数据字典

---
+++ src/dictionary/index.js
@@ -0,0 +1,9 @@
+/**
+ * 数据字典
+ * @type {*[]}
+ * 用法实例：MANAGER.find(item_ => +item_.value == item.value).label
+ */
+export const MANAGER = [
+  {value: 0, label: '应用账户'},
+  {value: 1, label: '管理员账户'}
+//
