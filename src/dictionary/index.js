e5638018b3f8c528e8767e816de821e8294bab6d
Apache-Ra <summer.vip.20@qq.com>
2019-04-04 14:43:06 +0800

    数据字典

---
+++ src/dictionary/index.js
@@ -0,0 +1,14 @@
+/**
+ * 数据字典
+ * @type {*[]}
+ */
+export const XXX_STATUS = [
+  {value: 0, label: '未发送'},
+  {value: 1, label: '已发送'}
+]
+/**
+ * 使用方法：
+ * import {SEND_STATUS} from 'directory'
+ * XXX_STATUS.find(item_ => +item_.value == item.value).label,
+ */
+
