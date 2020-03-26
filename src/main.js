6dbe757eefd7666ebaf4c6c236209bbf82ee9332
Apache-Ra <summer.vip.20@qq.com>
2020-03-26 18:09:45 +0800

    增加babel-polyfill，支持IE

--- src/main.js
+++ src/main.js
@@ -1,39 +1,40 @@
-import Vue from 'vue'
-import FastClick from 'fastclick'
-import App from './App'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import FastClick from 'fastclick';
+import App from './App';
+import router from './router';
+import store from './store';
+import 'babel-polyfill';
 /*
  * 定义常量
  */
-let DOMAIN_NAME = ''
-let SERVER_NAME = ''
-let API_PREFIX = ''
+let DOMAIN_NAME = '';
+let SERVER_NAME = '';
+let API_PREFIX = '';
 /**
  * 开发环境和发布环境变量
  */
 if (process.env.NODE_ENV === 'development') {
-  DOMAIN_NAME = ''
-  SERVER_NAME = ''
-  API_PREFIX = ''
-  console.log('开发环境')
+  DOMAIN_NAME = '';
+  SERVER_NAME = '';
+  API_PREFIX = '';
+  console.log('开发环境');
 } else {
-  console.log('发布环境')
-  DOMAIN_NAME = ''
-  SERVER_NAME = ''
-  API_PREFIX = ''
+  console.log('发布环境');
+  DOMAIN_NAME = '';
+  SERVER_NAME = '';
+  API_PREFIX = '';
 }
-Vue.prototype.DOMAIN_NAME = DOMAIN_NAME
-Vue.prototype.SERVER_NAME = SERVER_NAME
-Vue.prototype.API_PREFIX = API_PREFIX
+Vue.prototype.DOMAIN_NAME = DOMAIN_NAME;
+Vue.prototype.SERVER_NAME = SERVER_NAME;
+Vue.prototype.API_PREFIX = API_PREFIX;
 /**
  *  日志输出开关
  */
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 /**
  *  点击延迟
  */
-FastClick.attach(document.body)
+FastClick.attach(document.body);
 /**
  *  创建实例
  */
